a3938c2e4a2ab37488a612285eec7192e759e27c
abhisekumarjha <Abhishek.chhatauni@gmail.com>
2024-05-31 18:51:07 +0530

    Initial commit: File Sharing Platform

---
+++ index.js
@@ -0,0 +1,163 @@
+const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#fileInput");
+const browseBtn = document.querySelector("#browseBtn");
+
+const bgProgress = document.querySelector(".bg-progress");
+const progressPercent = document.querySelector("#progressPercent");
+const progressContainer = document.querySelector(".progress-container");
+const progressBar = document.querySelector(".progress-bar");
+const status = document.querySelector(".status");
+
+const sharingContainer = document.querySelector(".sharing-container");
+const copyURLBtn = document.querySelector("#copyURLBtn");
+const fileURL = document.querySelector("#fileURL");
+const emailForm = document.querySelector("#emailForm");
+
+const toast = document.querySelector(".toast");
+
+const uploadURL = "https://file.io"; // Updated to use File.io API
+const maxAllowedSize = 100 * 1024 * 1024; // 100MB
+
+browseBtn.addEventListener("click", () => {
+  fileInput.click();
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const files = e.dataTransfer.files;
+  if (files.length === 1) {
+    if (files[0].size < maxAllowedSize) {
+      fileInput.files = files;
+      uploadFile();
+    } else {
+      showToast("Max file size is 100MB");
+    }
+  } else if (files.length > 1) {
+    showToast("You can't upload multiple files");
+  }
+  dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZone.classList.add("dragged");
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+  dropZone.classList.remove("dragged");
+});
+
+fileInput.addEventListener("change", () => {
+  if (fileInput.files[0].size > maxAllowedSize) {
+    showToast("Max file size is 100MB");
+    fileInput.value = ""; // reset the input
+    return;
+  }
+  uploadFile();
+});
+
+copyURLBtn.addEventListener("click", () => {
+  fileURL.select();
+  document.execCommand("copy");
+  showToast("Copied to clipboard");
+});
+
+fileURL.addEventListener("click", () => {
+  fileURL.select();
+});
+
+const uploadFile = () => {
+  const files = fileInput.files;
+  const formData = new FormData();
+  formData.append("file", files[0]); // Changed to match file.io's expected form field name
+
+  // Show the uploader
+  progressContainer.style.display = "block";
+
+  // Upload file
+  const xhr = new XMLHttpRequest();
+
+  // Listen for upload progress
+  xhr.upload.onprogress = function (event) {
+    if (event.lengthComputable) {
+      let percent = Math.round((event.loaded / event.total) * 100);
+      progressPercent.innerText = percent;
+      bgProgress.style.width = `${percent}%`;
+      progressBar.style.width = `${percent}%`;
+    }
+  };
+
+  // Handle error
+  xhr.upload.onerror = function () {
+    showToast(`Error in upload: ${xhr.status}.`);
+    fileInput.value = ""; // reset the input
+  };
+
+  // Listen for response which will give the link
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      onFileUploadSuccess(xhr.responseText);
+    }
+  };
+
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
+
+const onFileUploadSuccess = (res) => {
+  fileInput.value = ""; // reset the input
+  status.innerText = "Uploaded";
+
+  // Parse the response to get the file URL
+  const response = JSON.parse(res);
+  const url = response.link;
+
+  // Remove the disabled attribute from form btn & make text send
+  emailForm[2].removeAttribute("disabled");
+  emailForm[2].innerText = "Send";
+  progressContainer.style.display = "none"; // hide the box
+
+  sharingContainer.style.display = "block";
+  fileURL.value = url;
+};
+
+emailForm.addEventListener("submit", (e) => {
+  e.preventDefault(); // stop submission
+
+  // Disable the button
+  emailForm[2].setAttribute("disabled", "true");
+  emailForm[2].innerText = "Sending";
+
+  const url = fileURL.value;
+  const formData = {
+    uuid: url.split("/").splice(-1, 1)[0],
+    emailTo: emailForm.elements["to-email"].value,
+    emailFrom: emailForm.elements["from-email"].value,
+  };
+
+  fetch(emailURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
+        showToast("Email Sent");
+        sharingContainer.style.display = "none"; // hide the box
+      }
+    });
+});
+
+let toastTimer;
+// The toast function
+const showToast = (msg) => {
+  clearTimeout(toastTimer);
+  toast.innerText = msg;
+  toast.classList.add("show");
+  toastTimer = setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000);
+};
